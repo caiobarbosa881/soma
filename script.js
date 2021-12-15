da5d15f5e3658cd5df3a86535c99777298056840
Caio Barbosa <caiobarbosasoriano@gmail.com>
2021-12-15 05:29:18 -0300

    Visual improvement

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+var v1 = document.querySelector(".valor1").value;
+var v2 = document.querySelector(".valor2").value;
+
+
 function somar() {
     let v1 = document.querySelector(".valor1").value;
     let v2 = document.querySelector(".valor2").value;
-
     var resultado = parseInt(v1) + parseInt(v2);
-    document.querySelector(".resultado").innerHTML = resultado;
+    document.querySelector("#telah1").innerHTML = resultado;
+}
+function multiplicar() {
+    let v1 = document.querySelector(".valor1").value;
+    let v2 = document.querySelector(".valor2").value;
+    var resultado = parseInt(v1) * parseInt(v2);
+    document.querySelector("#telah1").innerHTML = resultado;
+}
+function subtrair() {
+    let v1 = document.querySelector(".valor1").value;
+    let v2 = document.querySelector(".valor2").value;
+    var resultado = parseInt(v1) - parseInt(v2);
+    document.querySelector("#telah1").innerHTML = resultado;
+}
+function dividir() {
+    let v1 = document.querySelector(".valor1").value;
+    let v2 = document.querySelector(".valor2").value;
+    var resultado = parseInt(v1) / parseInt(v2);
+    document.querySelector("#telah1").innerHTML = resultado;
 }
